09c8bae44ddda42baf3806ba5fb1c9a1d03e35a9
Vladimir Fibe <motiw@me.com>
2024-11-19 20:12:34 +0500

    ProfileViewController

---
+++ EventHubApp/EventHubApp/Screens/ProfileViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ProfileViewController.swift
+//  EventHubApp
+//
+//  Created by Vladimir Fibe on 19.11.2024.
+//
+
+import UIKit
+
+final class ProfileViewController: UITableViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        navigationItem.title = "Profile"
+    }
+}
