72e93e813ea470577cae3ab421fe26b58f3fe73c
Dmitry <mrsahadov@gmail.com>
2024-11-23 10:05:57 -0300

    Refactor for MVP pattern

--- EventHubApp/EventHubApp/Screens/FavouritesScreen/FavouritesPresenter.swift
+++ EventHubApp/EventHubApp/Screens/FavouritesScreen/FavouritesPresenter.swift
@@ -9,68 +9,44 @@
 import Foundation
 
 protocol FavouritesViewOutput: AnyObject {
+    func loadEvents()
     func goToSearch()
 }
 
 class FavouritesPresenter {
 
+    // MARK: - Properties
     weak var viewInput: FavouritesViewInput?
-
+    private var events: [MyEvent] = []
+    
+    // MARK: - Initializer
     init(viewInput: FavouritesViewInput? = nil) {
         self.viewInput = viewInput
     }
 }
 
+// MARK: - FavouritesViewOutput
 extension FavouritesPresenter: FavouritesViewOutput {
-    func goToSearch() {
+    func loadEvents() {
+        // Simulate loading data (could fetch from a database or API)
+        events = [
+            MyEvent(date: "11 November", title: "The Big Bang Theory", place: "Moscow"),
+            MyEvent(date: "12 November", title: "Event Horizon", place: "Saint Petersburg"),
+            MyEvent(date: "13 November", title: "Cosmic Journey", place: "Novosibirsk")
+        ]
+        
+        viewInput?.updateEvents(events)
 
+        if events.isEmpty {
+            viewInput?.showNoBookmarksMessage()
+        } else {
+            viewInput?.hideNoBookmarksMessage()
+            viewInput?.updateEvents(events)
+        }
+    }
+
+    func goToSearch() {
+        print("Search action triggered")
+        viewInput?.onSearchTapped()
     }
 }
-
-
-//import Foundation
-//
-//protocol FavouritesViewOutput: AnyObject {
-//    func loadEvents()
-//    func goToSearch()
-//}
-//
-//class FavouritesPresenter {
-//
-//    // MARK: - Properties
-//    weak var viewInput: FavouritesViewInput?
-//    private var events: [MyEvent] = [] // Manage events in the presenter
-//
-//    // MARK: - Initializer
-//    init(viewInput: FavouritesViewInput? = nil) {
-//        self.viewInput = viewInput
-//    }
-//}
-//
-//// MARK: - FavouritesViewOutput
-//extension FavouritesPresenter: FavouritesViewOutput {
-//    func loadEvents() {
-//        // Simulate loading data (could fetch from a database or API)
-//        events = [
-//            MyEvent(date: "11 November", title: "The Big Bang Theory", place: "Moscow"),
-//            MyEvent(date: "12 November", title: "Event Horizon", place: "Saint Petersburg"),
-//            MyEvent(date: "13 November", title: "Cosmic Journey", place: "Novosibirsk")
-//        ]
-//
-//        // Notify the view about the data
-//        if events.isEmpty {
-//            viewInput?.showNoBookmarksMessage()
-//        } else {
-//            viewInput?.hideNoBookmarksMessage()
-//            viewInput?.updateEvents(events)
-//        }
-//    }
-//
-//    func goToSearch() {
-//        // Handle search action
-//        print("Search action triggered")
-//        viewInput?.onSearchTapped()
-//    }
-//}
-//
-//
--- EventHubApp/EventHubApp/Screens/FavouritesScreen/FavouritesViewController.swift
+++ EventHubApp/EventHubApp/Screens/FavouritesScreen/FavouritesViewController.swift
@@ -14,7 +14,12 @@ struct MyEvent {
     let place: String
 }
 
+
+// MARK: - Protocol for View Input
 protocol FavouritesViewInput: AnyObject {
+    func updateEvents(_ events: [MyEvent])
+    func showNoBookmarksMessage()
+    func hideNoBookmarksMessage()
     func onSearchTapped()
 }
 
@@ -22,54 +27,45 @@ class FavouritesViewController: UIViewController {
 
     // MARK: - Properties
     var viewOutput: FavouritesViewOutput!
-
-
-    let eventsArray = [MyEvent(date: "11 November", title: "The big bang theory", place: "Moscow"),
-                       MyEvent(date: "11 November", title: "The big bang theory", place: "Moscow"),
-                       MyEvent(date: "11 November", title: "The big bang theory", place: "Moscow"),
-                       MyEvent(date: "11 November", title: "The big bang theory", place: "Moscow")]
-
+    private var eventsArray: [MyEvent] = []
 
     // MARK: - Views
-    var collectionView: UICollectionView!
+    private var collectionView: UICollectionView!
 
-    let titleLabel: UILabel = {
-            let title = UILabel()
-            title.translatesAutoresizingMaskIntoConstraints = false
-            title.textAlignment = .center
-            title.tintColor = AppColors.black
-            title.text = "Favorites"
-            title.font = .airbnbFont(ofSize: 26, weight: .book)
-            return title
+    private let titleLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textAlignment = .center
+        label.tintColor = AppColors.black
+        label.text = "Favorites"
+        label.font = .airbnbFont(ofSize: 26, weight: .book)
+        return label
     }()
 
-    let searchImageView: UIImageView = {
-        let image = UIImageView()
-        image.translatesAutoresizingMaskIntoConstraints = false
-        image.image = UIImage(named: "magnifying-glass")
-        image.contentMode = .scaleAspectFill
-        return image
+    private let searchImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.image = UIImage(named: "magnifying-glass")
+        imageView.contentMode = .scaleAspectFill
+        return imageView
     }()
 
-    let noBookmarksLabel: UILabel = {
-            let title = UILabel()
-            title.translatesAutoresizingMaskIntoConstraints = false
-            title.textAlignment = .center
-            title.numberOfLines = 0
-            title.tintColor = .systemGray6
-            title.text = "You haven't saved any articles yet. Start reading and bookmarking them now."
-            title.font = UIFont.systemFont(ofSize: 18, weight: .regular)
-            return title
+    private let noBookmarksLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textAlignment = .center
+        label.numberOfLines = 0
+        label.tintColor = .systemGray6
+        label.text = "You haven't saved any articles yet. Start reading and bookmarking them now."
+        label.font = UIFont.systemFont(ofSize: 18, weight: .regular)
+        label.isHidden = true
+        return label
     }()
 
-    // MARK: - Initializers
-    init(viewOutput: FavouritesViewOutput){
+    // MARK: - Initializer
+    init(viewOutput: FavouritesViewOutput) {
         super.init(nibName: nil, bundle: nil)
         self.viewOutput = viewOutput
-        setupViews()
-        setCollectionView()
-        loadBookmarkdData()
-        setConstraints()
     }
 
     required init?(coder: NSCoder) {
@@ -79,47 +75,51 @@ class FavouritesViewController: UIViewController {
     // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        setupLayout()
-    }
-
-    func searchPressed(){
-        print("Facebook pressed")
-    }
-
-    private func setupViews() {
-        view.backgroundColor = .white
-    }
-
-    private func loadBookmarkdData() {
-        let isShown = true
-
-        if isShown {
-            noBookmarksLabel.isHidden = true
-        } else {
-            collectionView.isHidden = true
-        }
-    }
-
-}
-
-// MARK: - Layout
-private extension FavouritesViewController {
-    func setupLayout(){
-        view.addSubview(titleLabel)
-        view.addSubview(noBookmarksLabel)
-        view.addSubview(searchImageView)
-        view.backgroundColor = .red
+        setupViews()
+        setCollectionView()
+        setConstraints()
+        viewOutput.loadEvents()
+        
+        // Fix data loading
+        eventsArray = [
+                MyEvent(date: "11 November", title: "The Big Bang Theory", place: "Moscow"),
+                MyEvent(date: "12 November", title: "Event Horizon", place: "Saint Petersburg"),
+                MyEvent(date: "13 November", title: "Cosmic Journey", place: "Novosibirsk")
+        ]
     }
 }
 
-// MARK: - LoginViewInput Delegate
+// MARK: - FavouritesViewInput Implementation
 extension FavouritesViewController: FavouritesViewInput {
-    func onSearchTapped() {
+    func updateEvents(_ events: [MyEvent]) {
+        eventsArray = events
+        collectionView.reloadData()
+    }
 
+    func showNoBookmarksMessage() {
+        noBookmarksLabel.isHidden = false
+        collectionView.isHidden = true
+    }
+
+    func hideNoBookmarksMessage() {
+        noBookmarksLabel.isHidden = true
+        collectionView.isHidden = false
+    }
+
+    func onSearchTapped() {
+        print("Search tapped")
     }
 }
 
+// MARK: - Private Methods
 private extension FavouritesViewController {
+    func setupViews() {
+        view.backgroundColor = .white
+        view.addSubview(titleLabel)
+        view.addSubview(searchImageView)
+        view.addSubview(noBookmarksLabel)
+    }
+
     func setCollectionView() {
         let layout = UICollectionViewFlowLayout()
         layout.minimumLineSpacing = 20
@@ -128,23 +128,25 @@ private extension FavouritesViewController {
         collectionView.dataSource = self
         collectionView.delegate = self
         collectionView.register(FavouriteCell.self, forCellWithReuseIdentifier: "cell")
+        collectionView.backgroundColor = .clear
         view.addSubview(collectionView)
     }
-}
 
-private extension FavouritesViewController {
     func setConstraints() {
         NSLayoutConstraint.activate([
             titleLabel.topAnchor.constraint(equalTo: view.topAnchor, constant: 50),
             titleLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+
             searchImageView.topAnchor.constraint(equalTo: view.topAnchor, constant: 52),
             searchImageView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -25),
             searchImageView.widthAnchor.constraint(equalToConstant: 33),
             searchImageView.heightAnchor.constraint(equalToConstant: 33),
-            collectionView.topAnchor.constraint(equalTo: titleLabel.topAnchor, constant: 50),
+
+            collectionView.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 20),
             collectionView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -50),
-            collectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
             collectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            collectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+
             noBookmarksLabel.centerYAnchor.constraint(equalTo: view.centerYAnchor),
             noBookmarksLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 30),
             noBookmarksLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -30)
@@ -152,6 +154,7 @@ private extension FavouritesViewController {
     }
 }
 
+// MARK: - UICollectionViewDataSource & Delegate
 extension FavouritesViewController: UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return eventsArray.count
@@ -184,161 +187,3 @@ extension FavouritesViewController: UICollectionViewDataSource, UICollectionView
             return 20
     }
 }
-
-//import UIKit
-//
-//// MARK: - Protocol for View Input
-//protocol FavouritesViewInput: AnyObject {
-//    func updateEvents(_ events: [MyEvent])
-//    func showNoBookmarksMessage()
-//    func hideNoBookmarksMessage()
-//    func onSearchTapped()
-//}
-//
-//class FavouritesViewController: UIViewController {
-//
-//    // MARK: - Properties
-//    var viewOutput: FavouritesViewOutput! // Presenter
-//    private var eventsArray: [MyEvent] = []
-//
-//    // MARK: - Views
-//    private var collectionView: UICollectionView!
-//
-//    private let titleLabel: UILabel = {
-//        let label = UILabel()
-//        label.translatesAutoresizingMaskIntoConstraints = false
-//        label.textAlignment = .center
-//        label.tintColor = AppColors.black
-//        label.text = "Favorites"
-//        label.font = .airbnbFont(ofSize: 26, weight: .book)
-//        return label
-//    }()
-//
-//    private let searchImageView: UIImageView = {
-//        let imageView = UIImageView()
-//        imageView.translatesAutoresizingMaskIntoConstraints = false
-//        imageView.image = UIImage(named: "magnifying-glass")
-//        imageView.contentMode = .scaleAspectFill
-//        return imageView
-//    }()
-//
-//    private let noBookmarksLabel: UILabel = {
-//        let label = UILabel()
-//        label.translatesAutoresizingMaskIntoConstraints = false
-//        label.textAlignment = .center
-//        label.numberOfLines = 0
-//        label.tintColor = .systemGray6
-//        label.text = "You haven't saved any articles yet. Start reading and bookmarking them now."
-//        label.font = UIFont.systemFont(ofSize: 18, weight: .regular)
-//        label.isHidden = true // Initially hidden
-//        return label
-//    }()
-//
-//    // MARK: - Initializer
-//    init(viewOutput: FavouritesViewOutput) {
-//        super.init(nibName: nil, bundle: nil)
-//        self.viewOutput = viewOutput
-//    }
-//
-//    required init?(coder: NSCoder) {
-//        fatalError("init(coder:) has not been implemented")
-//    }
-//
-//    // MARK: - Lifecycle
-//    override func viewDidLoad() {
-//        super.viewDidLoad()
-//        setupViews()
-//        setCollectionView()
-//        setConstraints()
-//        viewOutput.loadEvents() // Request data from Presenter
-//    }
-//}
-//
-//// MARK: - FavouritesViewInput Implementation
-//extension FavouritesViewController: FavouritesViewInput {
-//    func updateEvents(_ events: [MyEvent]) {
-//        eventsArray = events
-//        collectionView.reloadData()
-//    }
-//
-//    func showNoBookmarksMessage() {
-//        noBookmarksLabel.isHidden = false
-//        collectionView.isHidden = true
-//    }
-//
-//    func hideNoBookmarksMessage() {
-//        noBookmarksLabel.isHidden = true
-//        collectionView.isHidden = false
-//    }
-//
-//    func onSearchTapped() {
-//        // Handle search action
-//        print("Search tapped")
-//    }
-//}
-//
-//// MARK: - Private Methods
-//private extension FavouritesViewController {
-//    func setupViews() {
-//        view.backgroundColor = .white
-//        view.addSubview(titleLabel)
-//        view.addSubview(searchImageView)
-//        view.addSubview(noBookmarksLabel)
-//    }
-//
-//    func setCollectionView() {
-//        let layout = UICollectionViewFlowLayout()
-//        layout.minimumLineSpacing = 20
-//        collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
-//        collectionView.translatesAutoresizingMaskIntoConstraints = false
-//        collectionView.dataSource = self
-//        collectionView.delegate = self
-//        collectionView.register(FavouriteCell.self, forCellWithReuseIdentifier: "cell")
-//        collectionView.backgroundColor = .clear
-//        view.addSubview(collectionView)
-//    }
-//
-//    func setConstraints() {
-//        NSLayoutConstraint.activate([
-//            titleLabel.topAnchor.constraint(equalTo: view.topAnchor, constant: 50),
-//            titleLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-//
-//            searchImageView.topAnchor.constraint(equalTo: view.topAnchor, constant: 52),
-//            searchImageView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -25),
-//            searchImageView.widthAnchor.constraint(equalToConstant: 33),
-//            searchImageView.heightAnchor.constraint(equalToConstant: 33),
-//
-//            collectionView.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 20),
-//            collectionView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -50),
-//            collectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-//            collectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-//
-//            noBookmarksLabel.centerYAnchor.constraint(equalTo: view.centerYAnchor),
-//            noBookmarksLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 30),
-//            noBookmarksLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -30)
-//        ])
-//    }
-//}
-//
-//// MARK: - UICollectionViewDataSource & Delegate
-//extension FavouritesViewController: UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
-//    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-//        return eventsArray.count
-//    }
-//
-//    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-//        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! FavouriteCell
-//        cell.configure(with: eventsArray[indexPath.row])
-//        return cell
-//    }
-//
-//    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-//        let padding: CGFloat = 40
-//        let availableWidth = collectionView.frame.width - padding
-//        return CGSize(width: availableWidth, height: 112)
-//    }
-//
-//    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
-//        return UIEdgeInsets(top: 10, left: 20, bottom: 10, right: 20)
-//    }
-//}
